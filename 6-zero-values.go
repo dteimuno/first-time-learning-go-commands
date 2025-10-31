a65bdaaa0371ebc3d3fd9e974a0528a4ebbb8e1e
Dennis Tei-Muno <munodennis@gmail.com>
2025-10-31 04:23:54 -0500

    Add example of zero values in Go
    Demonstrate zero values for different data types in Go.

---
+++ 6-zero-values.go
@@ -0,0 +1,25 @@
+// if you do not assign a value to a variable, the value is considered zero
+//Data Types: bool int float64 string
+/* Examples:
+bool = false
+int = 0
+float64 = 0.0
+string = ""
+*/
+
+//zero values int
+package main
+import "fmt"
+func main() {
+  var i int
+  fmt.Printf("%d", i)
+}
+// value printed will be zero
+
+//zero values float
+package main
+import "fmt"
+func main() {
+  var fl float64
+  fmt.Printf("%d", fl)
+}
